aa09899f18676111197f77e4e646d641e0e4c3ff
ayunita <ayunita.dev@gmail.com>
2022-05-24 16:01:13 +0300

    fixed linter

--- index.js
+++ index.js
@@ -59,11 +59,11 @@ if (!inProduction) {
   app.get('/health', (req, res) => {
     res.send('ok')
   })
-  
+
   app.get('/version', (req, res) => {
     res.send('1') // change this string to ensure a new version deployed
   })
-  
+
   app.get('*', (req, res) => res.sendFile(INDEX_PATH))
 }
 
